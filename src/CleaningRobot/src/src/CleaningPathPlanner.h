b98d45108df35551e9aa2f9e0f5e15b53eeaa24e
pl9632008 <pl9632008@gmail.com>
2024-10-14 23:33:01 -0700

    chore : rm nav_msgs/Odometry.h

--- src/CleaningRobot/src/src/CleaningPathPlanner.h
+++ src/CleaningRobot/src/src/CleaningPathPlanner.h
@@ -20,8 +20,6 @@
 #include <costmap_2d/costmap_2d_ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <nav_msgs/Path.h>
-#include <nav_msgs/Odometry.h>
-
 
 using namespace cv;
 using namespace std;
